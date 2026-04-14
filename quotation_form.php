2d5144d5a7178460987a642ec8114245b734e5f8
buildsbytanmay <dushantb123@gmail.com>
2026-04-14 22:50:06 +0530

    Added navbar to quatation form

---
+++ quotation_form.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Quotation Form</title>
+    <link rel="stylesheet" href="quotation_form.css">
+    <link rel="stylesheet" href="nav.css">
+    <link rel="stylesheet" href="footer.css">
+</head>
+
+<body>
+
+    <nav>
+        <a href="index.html"><div class="logo"></div></a>
+
+        <!-- Hamburger -->
+        <div class="hamburger" onclick="toggleMenu()">☰</div>
+
+        <div class="menus">
+            <a class="submenus underline" href="index.html">HOME</a>
+            <a class="submenus underline" href="service.html">SERVICES</a>
+            <a class="submenus underline" href="project.html">PROJECTS</a>
+            <a class="submenus underline" href="about_us.html">ABOUT US</a>
+        </div>
+    </nav>
+
+</body>
+</html>
