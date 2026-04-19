d5a06dcf0fc78378fb7b7bfc153dab495c89946e
buildsbytanmay <dushantb123@gmail.com>
2026-04-19 23:52:55 +0530

    Modified quotation form

--- quotation_form.php
+++ quotation_form.php
@@ -112,5 +112,12 @@
         </div>
     </div>
 
+    <?php
+if (isset($_SESSION['alert'])) {
+    echo "<script>alert('{$_SESSION['alert']}');</script>";
+    unset($_SESSION['alert']);
+}
+?>
+
 </body>
 </html>
