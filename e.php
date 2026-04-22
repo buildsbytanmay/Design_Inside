c18dcbf1f36f674d22663ea57179273d998bea37
buildsbytanmay <dushantb123@gmail.com>
2026-04-22 23:59:04 +0530

    fetching form data

--- e.php
+++ e.php
@@ -6,6 +6,22 @@ if ($conn->connect_error) {
     die("Database connection failed: " . $conn->connect_error);
 }
 
+/* ---------- FETCH FORM DATA ---------- */
+$first_name = trim($_POST['first_name']);
+$last_name  = trim($_POST['last_name']);
+$name       = $first_name . " " . $last_name;
+
+$email      = trim($_POST['email']);
+$phone_num  = trim($_POST['phone_num']);
+$phone_num2 = trim($_POST['phone_num2']);
+
+$address    = trim($_POST['address']);
+$place      = trim($_POST['place']);
+$service    = trim($_POST['service']);
+
+$price_range = $_POST['price_range'];
+$start_date  = $_POST['start_date'];
+$message     = trim($_POST['message']);
 
 
 ?>
