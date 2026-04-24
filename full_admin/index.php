b409594116ac6dedb948099a3b5a8cf9437813a8
buildsbytanmay <dushantb123@gmail.com>
2026-04-24 23:46:49 +0530

    login form added

--- full_admin/index.php
+++ full_admin/index.php
@@ -15,5 +15,26 @@
 </head>
 <body>
 
+    <div id="container">
+
+        <!-- LOGIN FORM -->
+        <div class="login">
+            <div class="content">
+                <h1>Log In</h1>
+
+                <form method="POST" action="login.php">
+                    <input type="text" name="user_input" placeholder="Username / Email" required>
+                    <input type="password" name="password" placeholder="Password" required>
+                    <input type="hidden" name="login" value="1">
+
+                    <span class="clearfix"></span>
+
+                    <!-- ONLY submit -->
+                    <button type="submit">Log In</button>
+                </form>
+            </div>
+        </div>
+    </div>
+
 </body>
 </html>
