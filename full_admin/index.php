feb97b227181b57c1a7b9a1df980c301aabd9ee9
buildsbytanmay <dushantb123@gmail.com>
2026-04-24 23:44:36 +0530

    created index.php file

---
+++ full_admin/index.php
@@ -0,0 +1,19 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>ADMIN Login / Forgot Password</title>
+
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="nav.css">
+
+    <!-- VERY IMPORTANT -->
+    <script src="script.js" defer></script>
+</head>
+<body>
+
+</body>
+</html>
