71df8ffe5f8e2c1ce91e9599cbde45cc348dd732
buildsbytanmay <dushantb123@gmail.com>
2026-05-06 23:58:28 +0530

    navbar added

--- full_admin/admin/completed.php
+++ full_admin/admin/completed.php
@@ -24,3 +24,31 @@ body{
     background:#f5f5f5;
 }
 </style>
+</head>
+
+<body>
+
+<!-- <nav>
+    <div class="logo">Design_Inside</div>
+    <div>
+        <a href="dashboard.php">Home</a>
+        <a href="customer.php">Customer</a>
+        <a href="completed.php">Completed</a>
+        <a href="logout.php">Logout</a>
+    </div>
+</nav> -->
+<nav>
+    <a href="http://localhost/Design_Inside/">
+        <div class="logo"></div>
+    </a>
+
+    <!-- Hamburger -->
+    <div class="hamburger" onclick="toggleMenu()">☰</div>
+
+    <div class="menus">
+        <a class="submenus underline" href="dashboard.php">DASHBOARD</a>
+        <a class="submenus underline" href="customer.php">CUSTOMER</a>
+        <a class="submenus underline" href="completed.php">COMPLETED</a>
+        <a class="submenus underline" href="logout.php">LOGOUT</a>
+    </div>
+</nav>
