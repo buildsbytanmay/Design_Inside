1a3bb88db3e033503c82036e49b275019040a691
buildsbytanmay <dushantb123@gmail.com>
2026-05-08 23:01:07 +0530

    navbar added to customer.php

---
+++ full_admin/admin/customer.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>Customer Management</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<style>
+
+</style>
+</head>
+<body>
+    <!-- <nav>
+        <div class="logo">Design_Inside</div>
+        <div>
+            <a href="dashboard.php">Home</a>
+            <a href="customer.php">Customer</a>
+            <a href="completed.php">Completed</a>
+            <a href="logout.php">Logout</a>
+        </div>
+    </nav> -->
+    <nav>
+    <a href="http://localhost/Design_Inside/">
+        <div class="logo"></div>
+    </a>
+
+    <!-- Hamburger -->
+    <div class="hamburger" onclick="toggleMenu()">☰</div>
+
+    <div class="menus">
+        <a class="submenus underline" href="dashboard.php">DASHBOARD</a>
+        <a class="submenus underline" href="customer.php">CUSTOMER</a>
+        <a class="submenus underline" href="completed.php">COMPLETED</a>
+        <a class="submenus underline" href="logout.php">LOGOUT</a>
+    </div>
+</nav>
+
+
+
+</body>
+</html>
